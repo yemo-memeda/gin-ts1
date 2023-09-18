cd80e6a7faa02ccef44efbba101effd704f1160a
787743742@qq.com <adler.liu16@gmail.com>
2023-09-18 20:52:07 +0800

    add static and graceful stop

--- main.go
+++ main.go
@@ -3,21 +3,38 @@ package main
 import (
 	"gin-ts1/bootstrap"
 	"gin-ts1/global"
-	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
-// log test
-package main
-
-import (
-"github.com/gin-gonic/gin"
-"jassue-gin/bootstrap"
-"jassue-gin/global"
-"net/http"
-)
-
-//add db init tests
+// add db init tests
+//
+//	func main() {
+//		// 初始化配置
+//		bootstrap.InitializeConfig()
+//
+//		// 初始化日志
+//		global.App.Log = bootstrap.InitializeLog()
+//		global.App.Log.Info("log init success!")
+//
+//		// 初始化数据库
+//		global.App.DB = bootstrap.InitializeDB()
+//		// 程序关闭前，释放数据库连接
+//		defer func() {
+//			if global.App.DB != nil {
+//				db, _ := global.App.DB.DB()
+//				db.Close()
+//			}
+//		}()
+//
+//		r := gin.Default()
+//
+//		// 测试路由
+//		r.GET("/ping", func(c *gin.Context) {
+//			c.String(http.StatusOK, "pong")
+//		})
+//
+//		// 启动服务器
+//		r.Run(":" + global.App.Config.App.Port)
+//	}
 func main() {
 	// 初始化配置
 	bootstrap.InitializeConfig()
@@ -36,14 +53,6 @@ func main() {
 		}
 	}()
 
-	r := gin.Default()
-
-	// 测试路由
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
 	// 启动服务器
-	r.Run(":" + global.App.Config.App.Port)
+	bootstrap.RunServer()
 }
-
